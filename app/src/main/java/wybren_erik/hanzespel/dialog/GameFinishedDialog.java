ef0c12d458c59060cf35c3813796de2cba84c6ff
Ploffie <eriklaan2@gmail.com>
2017-09-08 14:07:34 +0200

    Changed end of game procedure.

--- app/src/main/java/wybren_erik/hanzespel/dialog/GameFinishedDialog.java
+++ app/src/main/java/wybren_erik/hanzespel/dialog/GameFinishedDialog.java
@@ -15,38 +15,38 @@ import android.support.v7.app.AlertDialog;
 import wybren_erik.hanzespel.model.Boat;
 import wybren_erik.hanzespel.model.InventoryModel;
 
-/**
- * Created by wybrenoppedijk on 23/08/2017.
- */
-
 public class GameFinishedDialog extends DialogFragment {
     private InventoryModel inv = InventoryModel.getInstance();
     private Uri intervention = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
-    private Ringtone r;
 
     @NonNull
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
         AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
-        r = RingtoneManager.getRingtone(getContext(), intervention);
+        Ringtone r = RingtoneManager.getRingtone(getContext(), intervention);
         r.play();
         if (Boat.getInstance().getLocation().toString().equals("Kampen")) {
-            builder.setMessage("Het spel is afgelopen. Goed gedaan! U heeft Kampen gehaald met een bedrag van: " + InventoryModel.getInstance().getMoney() + "Ð.").setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            Boat.getInstance().sink(); // Destroy boat
+            builder.setMessage("Het spel is afgelopen. Goed gedaan! U heeft Kampen gehaald met een bedrag van Ð " + InventoryModel.getInstance().getMoney() + ".\n" +
+                    "Meld de naam van uw boot en uw eindbedrag bij de spelleider voordat u op OK klikt!")
+                    .setPositiveButton("OK", new DialogInterface.OnClickListener() {
                 @Override
                 public void onClick(DialogInterface dialog, int which) {
 
                 }
             });
         } else {
+            Boat.getInstance().sink(); // Destroy boat
             inv.setMoney(((inv.getMoney() / 5) * 4));
-            builder.setMessage("Het spel is afgelopen. Helaas heeft u Kampen niet gehaald. Daarvoor krijgt u een boete. van 1000Ð, Uw eindbedrag is " + InventoryModel.getInstance().getMoney() + "Ð.").setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            builder.setMessage("Het spel is afgelopen. Helaas heeft u Kampen niet gehaald. Daarvoor krijgt u een boete, Uw eindbedrag is Ð " + InventoryModel.getInstance().getMoney() + ".\n" +
+                    "Meld de naam van uw boot en uw eindbedrag bij de spelleider voordat u op OK klikt!")
+                    .setPositiveButton("OK", new DialogInterface.OnClickListener() {
                 @Override
                 public void onClick(DialogInterface dialog, int which) {
 
                 }
             });
         }
-        Boat.getInstance().sink(); // Destroy boat
         return builder.create();
     }
 }
